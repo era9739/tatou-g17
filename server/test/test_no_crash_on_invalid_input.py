4362fed37f85eeb53ace01e7d27d9982042b8d9a
HarinW_SU <96050782+HWdreamOUSL@users.noreply.github.com>
2025-10-17 20:46:10 +0200

    Delete server/test/test_no_crash_on_invalid_input.py

--- server/test/test_no_crash_on_invalid_input.py
+++
@@ -1,9 +0,0 @@
-# #test_no_crash_on_invalid_input.py
-# import requests
-#
-# BASE_URL = "http://localhost:5000"
-#
-# def test_invalid_login_payload_does_not_crash():
-#     payload = {"login": "<script>alert(1)</script>", "password": "", "email": "x@y"}
-#     resp = requests.post(f"{BASE_URL}/login", json=payload)
-#     assert resp.status_code in [400, 405, 404]
